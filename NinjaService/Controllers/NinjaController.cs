72e885f455bc8ad4e70391fdd9ee3242e93aa6f1
Fredrik Leijon <fredrik.leijon@gmail.com>
2011-10-08 10:08:47 +0200

    mock service

---
+++ NinjaService/Controllers/NinjaController.cs
@@ -0,0 +1,17 @@
+﻿using System.Web.Mvc;
+using NinjaService.Models;
+
+namespace NinjaService.Controllers
+{
+    public class NinjaController : Controller
+    {
+        //
+        // GET: /Njnja/
+        public JsonResult Index()
+        {
+            
+            return Json(new Ninja(){Name ="Ninja Ninjasson", Phone = "0701 13 13 13"}, JsonRequestBehavior.AllowGet);
+        }
+
+    }
+}
---
+++ NinjaService/Models/Ninja.cs
@@ -0,0 +1,8 @@
+﻿namespace NinjaService.Models
+{
+    public class Ninja
+    {
+        public string Name { get; set; }
+        public string Phone { get; set; }
+    }
+}
