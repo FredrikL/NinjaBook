20846cf7ac2487b8428c4102bfcef640046ba11a
Fredrik Leijon <fredrik.leijon@gmail.com>
2011-10-08 10:55:34 +0200

    Wired up autofact + dummy repo

---
+++ NinjaService/App/IAllNinjas.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using NinjaService.Models;
+
+namespace NinjaService.App
+{
+    public interface IAllNinjas
+    {
+        IEnumerable<Ninja> GetAll();
+
+        Ninja Edit(Ninja ninja);
+    }
+}
