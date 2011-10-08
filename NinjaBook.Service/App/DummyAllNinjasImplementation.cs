dd75f3524bd72e9885fdeb8c389e3540116819f9
TrimWeb <jim@tretton37.com>
2011-10-08 13:48:34 +0200

    added request and dummy data

--- NinjaBook.Service/App/DummyAllNinjasImplementation.cs
+++ NinjaBook.Service/App/DummyAllNinjasImplementation.cs
@@ -9,7 +9,19 @@ namespace NinjaBook.Service.App
     {
         public IEnumerable<Ninja> GetAll()
         {
-            return Enumerable.Empty<Ninja>();
+          return new[]
+                   {
+                     GetByNickName(""),
+                     new Ninja(){Nickname = "nickname", Name = "Ninja Ninjasson", Phone="123456"},
+                     new Ninja(){Nickname = "nickname", Name = "Ninja Ninjasson1", Phone="1234561"},
+                     new Ninja(){Nickname = "nickname", Name = "Ninja Ninjasson2", Phone="1234562"},
+                     new Ninja(){Nickname = "nickname", Name = "Ninja Ninjasson3", Phone="1234563"},
+                     new Ninja(){Nickname = "nickname", Name = "Ninja Ninjasson4", Phone="1234564"},
+                     new Ninja(){Nickname = "nickname", Name = "Ninja Ninjasson5", Phone="1234565"},
+                     new Ninja(){Nickname = "nickname", Name = "Ninja Ninjasson6", Phone="1234566"},
+                     new Ninja(){Nickname = "nickname", Name = "Ninja Ninjasson7", Phone="1234567"},
+                     new Ninja(){Nickname = "nickname", Name = "Ninja Ninjasson8", Phone="1234568"},
+                   };
         }
 
         public Ninja Edit(Ninja ninja)
